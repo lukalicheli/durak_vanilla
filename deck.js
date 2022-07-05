3a23cb103f4cf59b1fe8d07dd26feeadbeae20af
Luka Licheli <lukalicheli@gmail.com>
2022-07-04 17:42:01 -0700

    Created deck and shuffling functions

---
+++ deck.js
@@ -0,0 +1,27 @@
+function createDeck() {
+    let suits = ['H', 'C', 'D', 'S'];
+    let ranks = ['6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    let deck = [];
+
+    for(let i = 0; i < suits.length; i++) {
+        for(let j = 0; j<ranks.length; j++) {
+            deck.push(ranks[j] + suits[i]);
+        }
+    }
+    return deck;
+}
+
+function shuffleDeck(deck) {
+    for(let i=0; i<deck.length; i++) {
+        let tempCard = deck[i];
+        let randomIndex = Math.floor(Math.random() * deck.length);
+        deck[i] = deck[randomIndex];
+        deck[randomIndex] = tempCard;
+    }
+}
+
+let newDeck = createDeck();
+shuffleDeck(newDeck); 
+console.log(newDeck);
+
+let newHand = newDeck.splice(0,6)
