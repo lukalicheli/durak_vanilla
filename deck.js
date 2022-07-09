9496ee05f8cc9d7f510e0ec7a8fdd4355b85e6e2
Luka Licheli <lukalicheli@gmail.com>
2022-07-08 22:03:53 -0700

    Fixed code

--- deck.js
+++ deck.js
@@ -1,33 +1,76 @@
-function createDeck() {
-    let suits = ['♥', '♣', '♦', '♤'];
-    let ranks = ['6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-    let deck = [];
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
 
-    for(let i = 0; i < suits.length; i++) {
-        for(let j = 0; j<ranks.length; j++) {
-            deck.push(ranks[j] + suits[i]);
-        }
+export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards
+  }
+
+  get numberOfCards() {
+    return this.cards.length
+  }
+
+  // pop() {
+  //   return this.cards.shift()
+  // }
+
+  // push(card) {
+  //   this.cards.push(card)
+  // }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1))
+      const oldValue = this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
+      this.cards[i] = oldValue
     }
-    return deck;
+  }
 }
 
-function shuffleDeck(deck) {
-    for(let i=0; i<deck.length; i++) {
-        let tempCard = deck[i];
-        let randomIndex = Math.floor(Math.random() * deck.length);
-        deck[i] = deck[randomIndex];
-        deck[randomIndex] = tempCard;
-    }
+class Card {
+  constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+  }
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div")
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
 }
 
-let newDeck = createDeck();
-shuffleDeck(newDeck); 
-console.log(newDeck);
+function freshDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
+    })
+  })
+}
 
-let newHand1 = newDeck.splice(0,6);
-console.log(newHand1);
-console.log(newDeck);
+// let newDeck = createDeck();
 
-let newHand2 = newDeck.splice(0,6);
-console.log(newDeck);
-console.log(newHand2);
+// let newHand1 = newDeck.splice(0, 6);
+
+// let newHand2 = newDeck.splice(0, 6);
